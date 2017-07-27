806787d19c8a6126bd6badf66cc67e3455546504
莫锹文 <183795044@qq.com>
2017-07-27 09:14:39 +0800

    no message

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ require('NSURL');
 
 defineClass('MerusutoChristina.DataManager', {}, {
     getGithubURL: function(key) {
-        var path = "https://merusuto.oschina.io/data/";
+        var path = "https://merusuto.github.io/data/";
         return NSURL.URLWithString(path).URLByAppendingPathComponent(key);
     }
 })
